aba0cd987535657e8a42de050c662dff0aab84c5
Thalyson <thalyon.silva@ifce.edu.br>
2022-10-07 15:21:21 -0300

    [init] First commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,14 @@
+package br.com.dio.model;
+
+public class Gato {
+    private String nome;
+    private String cor;
+
+    public Gato() {
+    }
+
+    public Gato(String nome, String cor) {
+        this.nome = nome;
+        this.cor = cor;
+    }
+}
